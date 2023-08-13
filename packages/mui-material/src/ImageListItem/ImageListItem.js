19e2c1aeeda2c92d7968c1b8ff3f7ab54b101185
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-13 15:29:08 +0530

    Update ImageListItem.js
    Signed-off-by: Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>

--- packages/mui-material/src/ImageListItem/ImageListItem.js
+++ packages/mui-material/src/ImageListItem/ImageListItem.js
@@ -37,7 +37,7 @@ const ImageListItemRoot = styled('li', {
 })(({ ownerState }) => ({
   display: 'block',
   position: 'relative',
-  overflow: 'hidden',
+  //overflow: 'hidden',
   ...(ownerState.variant === 'standard' && {
     // For titlebar under list item
     display: 'flex',
